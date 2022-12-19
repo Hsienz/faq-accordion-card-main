ca3fa094ac8e0b0a97981a9ead49e4131c511e9f
Hsienz <hsieneis@gmail.com>
2022-12-19 23:40:58 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+const App = () => {
+  return (
+    <div>
+      <p className='font-sans font-bold text-Soft_red'>Hello world!</p>
+      <p className='font-bold'>Hello world!</p>
+    </div>
+  )
+}
+
+export default App
